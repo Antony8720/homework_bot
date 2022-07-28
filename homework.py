2ba644b8014804f95dc02cd7aaa124b596f28035
Anton Zlobin <mr.zlobin2015@yandex.ru>
2022-07-28 19:47:54 +0300

    Проект

--- homework.py
+++ homework.py
@@ -1,11 +1,18 @@
-...
+import requests
+import os
+import time
+import telegram
+
+from pprint import pprint
+from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,63 +27,74 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    """Отправляет сообщения в телеграм чат."""
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
+    """Делает запрос к эндпоинту."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    return homework_statuses.json()
 
 
 def check_response(response):
-
-    ...
+    """Проверяет ответ API на корректность."""
+    if type(response) == dict:
+        response = response.get('homeworks')
+        if response:
+            return response[0]
+        else:
+            pass
+    else:
+        pass
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    """Извлекает из информации о конкретной домашней работе статус этой
+    работы."""
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status not in HOMEWORK_STATUSES or homework_name is None:
+        raise Exception()
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    """Проверяет доступность переменных окружения."""
+    if (TELEGRAM_TOKEN is None
+       or TELEGRAM_CHAT_ID is None
+       or PRACTICUM_TOKEN is None):
+        return False
+    return True
 
 
 def main():
     """Основная логика работы бота."""
 
-    ...
+    try:
+        check_tokens()
+    except Exception as error:
+        logging.critical(f'Отсутствует переменная окружения! {error}')
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-
-    ...
-
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            message = parse_status(check_response(response)[0])
+            send_message(bot, message)
+            current_timestamp = response.get('current_date', current_timestamp)
             time.sleep(RETRY_TIME)
-
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            # current_error = message
+            # logging.error(message, exc_info=True)
+            # if current_error != message:
+            #     send_message(bot, message)
             time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
